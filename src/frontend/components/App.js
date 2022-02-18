3873dbf180ad2eaa72bf3fbfb995bca6c3ec9750
Ethan Cemer <tylercemer@gmail.com>
2022-02-18 17:17:41 -0600

    initial commit

---
+++ src/frontend/components/App.js
@@ -0,0 +1,49 @@
+
+import logo from './logo.png';
+import './App.css';
+ 
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a
+          className="navbar-brand col-sm-3 col-md-2 ms-3"
+          href="http://www.dappuniversity.com/bootcamp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dapp University
+        </a>
+      </nav>
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <main role="main" className="col-lg-12 d-flex text-center">
+            <div className="content mx-auto mt-5">
+              <a
+                href="http://www.dappuniversity.com/bootcamp"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={logo} className="App-logo" alt="logo"/>
+              </a>
+              <h1 className= "mt-5">Dapp University Starter Kit</h1>
+              <p>
+                Edit <code>src/components/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="http://www.dappuniversity.com/bootcamp"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LEARN BLOCKCHAIN <u><b>NOW! </b></u>
+              </a>
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
