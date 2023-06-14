c3661db4b410976d06430960c40ec1e67f66cffc
UPENDRA SINGH <upendra.singh@antiersolutions.com>
2023-06-14 21:30:10 +0530

    Replace Infura  IPFS Provider API with Pinata IPFS Provider API for IPFS integration

--- src/frontend/components/Create.js
+++ src/frontend/components/Create.js
@@ -1,65 +1,98 @@
+import axios from 'axios';
 import { useState } from 'react'
 import { ethers } from "ethers"
 import { Row, Form, Button } from 'react-bootstrap'
-import { create as ipfsHttpClient } from 'ipfs-http-client'
-const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
-
 const Create = ({ marketplace, nft }) => {
-  const [image, setImage] = useState('')
-  const [price, setPrice] = useState(null)
-  const [name, setName] = useState('')
-  const [description, setDescription] = useState('')
-  const uploadToIPFS = async (event) => {
-    event.preventDefault()
-    const file = event.target.files[0]
-    if (typeof file !== 'undefined') {
+  const [fileImg, setFile] = useState(null);
+  const [name, setName] = useState("")
+  const [desc, setDescription] = useState("")
+  const [price, setPrice] = useState("")
+  const sendJSONtoIPFS = async (ImgHash) => {
+
+    try {
+      const resJSON = await axios({
+        method: "post",
+        url: "https://api.pinata.cloud/pinning/pinJsonToIPFS",
+        data: {
+          "name": name,
+          "description": desc,
+          "image": ImgHash
+        },
+        headers: {
+          'pinata_api_key': process.env.REACT_APP_PINATA_API_KEY,
+          'pinata_secret_api_key': process.env.REACT_APP_PINATA_SECRET_API_KEY,
+
+        },
+      });
+
+      const tokenURI = `https://gateway.pinata.cloud/ipfs/${resJSON.data.IpfsHash}`;
+      console.log("Token URI", tokenURI);
+      mintThenList(tokenURI)
+    } catch (error) {
+      console.log("JSON to IPFS: ")
+      console.log(error);
+    }
+  }
+
+  const sendFileToIPFS = async (e) => {
+
+    e.preventDefault();
+
+    console.log(e);
+
+
+    if (fileImg) {
       try {
-        const result = await client.add(file)
-        console.log(result)
-        setImage(`https://ipfs.infura.io/ipfs/${result.path}`)
-      } catch (error){
-        console.log("ipfs image upload error: ", error)
+        const formData = new FormData();
+        formData.append("file", fileImg);
+
+        const resFile = await axios({
+          method: "post",
+          url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+          data: formData,
+          headers: {
+            'pinata_api_key': process.env.REACT_APP_PINATA_API_KEY,
+            'pinata_secret_api_key': process.env.REACT_APP_PINATA_SECRET_API_KEY,
+            "Content-Type": "multipart/form-data"
+          },
+        });
+
+        const ImgHash = `https://gateway.pinata.cloud/ipfs/${resFile.data.IpfsHash}`;
+        console.log(ImgHash);
+        sendJSONtoIPFS(ImgHash)
+
+
+      } catch (error) {
+        console.log("File to IPFS: ")
+        console.log(error)
       }
     }
   }
-  const createNFT = async () => {
-    if (!image || !price || !name || !description) return
-    try{
-      const result = await client.add(JSON.stringify({image, price, name, description}))
-      mintThenList(result)
-    } catch(error) {
-      console.log("ipfs uri upload error: ", error)
-    }
-  }
-  const mintThenList = async (result) => {
-    const uri = `https://ipfs.infura.io/ipfs/${result.path}`
+
+  const mintThenList = async (uri) => {
     // mint nft 
-    await(await nft.mint(uri)).wait()
+    await (await nft.mint(uri)).wait()
     // get tokenId of new nft 
     const id = await nft.tokenCount()
     // approve marketplace to spend nft
-    await(await nft.setApprovalForAll(marketplace.address, true)).wait()
+    await (await nft.setApprovalForAll(marketplace.address, true)).wait()
     // add nft to marketplace
     const listingPrice = ethers.utils.parseEther(price.toString())
-    await(await marketplace.makeItem(nft.address, id, listingPrice)).wait()
+    await (await marketplace.makeItem(nft.address, id, listingPrice)).wait()
   }
   return (
+
     <div className="container-fluid mt-5">
       <div className="row">
         <main role="main" className="col-lg-12 mx-auto" style={{ maxWidth: '1000px' }}>
           <div className="content mx-auto">
             <Row className="g-4">
-              <Form.Control
-                type="file"
-                required
-                name="file"
-                onChange={uploadToIPFS}
-              />
+              <Form.Control onChange={(e) => setFile(e.target.files[0])} size="lg" required type="file" name="file" />
               <Form.Control onChange={(e) => setName(e.target.value)} size="lg" required type="text" placeholder="Name" />
               <Form.Control onChange={(e) => setDescription(e.target.value)} size="lg" required as="textarea" placeholder="Description" />
               <Form.Control onChange={(e) => setPrice(e.target.value)} size="lg" required type="number" placeholder="Price in ETH" />
               <div className="d-grid px-0">
-                <Button onClick={createNFT} variant="primary" size="lg">
+                <Button onClick={sendFileToIPFS} variant="primary" size="lg">
                   Create & List NFT!
                 </Button>
               </div>
@@ -68,7 +101,8 @@ const Create = ({ marketplace, nft }) => {
         </main>
       </div>
     </div>
-  );
+
+  )
 }
 
 export default Create
